54eeb05be75c1296e8658fedce004eaa6c721597
just kadek <kdkteguhmahesa@gmail.com>
2025-08-04 21:35:13 +0800

    Add ESLint configuration with custom rules

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'indent': ['error', 2],
+      'comma-dangle': ['error', 'never'],
+      'no-trailing-spaces': 'error',
+      'eol-last': 'error'
+    }
+  }
+] 
